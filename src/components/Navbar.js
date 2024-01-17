d1dda74e98173639c85b9c9dc06f4882575e890d
Timmily <91490130+Timmilytim@users.noreply.github.com>
2024-01-17 23:26:16 +0100

    Fixed NavBar Error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,34 +15,41 @@ import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import { Link } from 'react-scroll';
+import { Link as ScrollLink } from 'react-scroll';
 
 export const Navbar = () => {
 
   const [openMenu, setOpenMenu] = useState(false)
-  const menuOptions = [
-    {
-      text: "Home",
-      icon: <HomeIcon/>
-    },
-    {
-      text: "About",
-      icon: <InfoIcon/>
-    },
-    {
-      text: "Purpose",
-      icon: <CommentRoundedIcon/>
-    },
-    {
-      text: "Contact",
-      icon: <PhoneRoundedIcon/>
-    }
-  ]
+ const menuOptions = [
+  {
+    text: "Home",
+    icon: <HomeIcon />,
+    linkTo: "home",
+  },
+  {
+    text: "About",
+    icon: <InfoIcon />,
+    linkTo: "about",
+  },
+  {
+    text: "Purpose",
+    icon: <CommentRoundedIcon />,
+    linkTo: "purpose",
+  },
+  {
+    text: "Contact",
+    icon: <PhoneRoundedIcon />,
+    linkTo: "contact",
+  },
+];
+
 
   return (
     <nav>
       <div className='nav-logo-container'>
         <img src ={Logo} alt = "alt" />
       </div>
+
       <div className='navbar-links-container'>
         <Link to='' spy={true} smooth={true} duration={1000}>Home</Link>
         <Link to='about' spy={true} smooth={true} duration={500}>About</Link>
@@ -50,26 +57,30 @@ export const Navbar = () => {
         <Link to='contact' spy={true} smooth={true} duration={1000}>Contact</Link>
         <Link to='https://x.com/cybernurture?t=TZT-zgQzKJq0IRMlSWesPw&s=09'><button className='primary-button'>Join Here</button></Link>
       </div>
+
       <div className='navbar-menu-container'>
         <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
       </div>
       <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor='right'>
-        <Box sx={{width: 250}}
-        role = "presentation"
+      <Box sx={{ width: 250 }}
+        role="presentation"
         onClick={() => setOpenMenu(false)}
-        onkeydown={() => setOpenMenu(false)}>
-          <List>
-            {menuOptions.map((item) =>(
-                <ListItem key ={item.text} disablePadding >
-                    <ListItemButton>
-                      <ListItemIcon>{item.icon}</ListItemIcon>
-                      <ListItemText primary={item.text} />
-                    </ListItemButton>
-                </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
+        onKeyDown={() => setOpenMenu(false)}
+      >
+        <List>
+          {menuOptions.map((item) => (
+            <ListItem key={item.text} disablePadding>
+              <ScrollLink to={item.linkTo} spy={true} smooth={true} duration={500}>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ScrollLink>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
     </nav>
   )
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { router } from './App';
-import { RouterProvider } from "react-router-dom";
+import App from './App';
+// import { RouterProvider } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
